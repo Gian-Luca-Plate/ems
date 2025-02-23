7e07a9e0c61c0f3f33c55b1a6af4a130c440d650
Gian-Luca-Plate <146996619+Gian-Luca-Plate@users.noreply.github.com>
2025-02-23 20:31:02 +0100

    s

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div style="display: flex; justify-content: center; margin-top: 10rem;">
+      <img src="../assets/google.png" alt="" style="width: 300px;">
+    </div>
+    <br>
+    <div style="display: flex; justify-content: center;">
+      <input type="text" v-model="x" @keydown.enter="suchen()" style="width: 300px; height: 30px; border-radius: 50px; text-align: center;">
+    </div>
+    <br>
+    <div style="display: flex; justify-content: center;">
+    <button style="width: 150px; height: 40px; text-align: center; font-weight: bolder; color: black;" @click="suchen()">mit Google Suchen</button>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+data() {
+  return {
+    x: ''
+  }
+},
+methods: {
+  suchen(){
+    if(this.x === 'Wer ist die beste Freundin der Welt'){
+      this.$router.push('/er')
+    }
+    if(this.x === 'hallo'){
+      alert('ich will sex ')
+    }
+  }
+},
+}
+</script>
---
+++ src/components/ergebnis.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <div style="display: flex; justify-content: center">
+      <input
+        type="text"
+        v-model="x"
+        @keydown.enter="suchen()"
+        style="
+          width: 300px;
+          height: 30px;
+          border-radius: 50px;
+          text-align: center;
+        "
+      />
+    </div>
+    <br />
+    <table>
+      <tbody>
+        <tr>
+          <td>
+            <img
+              src="../assets/r.png"
+              alt=""
+              style="width: 200px; border-radius: 8px"
+            />
+            <div >Wikepedia.de</div>
+          </td>
+          <td>
+            <br>
+            <img
+              src="../assets/t.png"
+              alt=""
+              style="width: 150px; border-radius: 8px"
+            />
+            <div>Pinterest.com</div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <img
+              src="../assets/u.png"
+              alt=""
+              style="width: 200px; border-radius: 8px; height: 200px;"
+            />
+            <div >Fbi.gor</div>
+          </td>
+          <td>
+            
+            <img
+              src="../assets/w.png"
+              alt=""
+              style="width: 150px; border-radius: 8px"
+            />
+            <div >rsplus.org.com</div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <img
+              src="../assets/e.png"
+              alt=""
+              style="width: 200px; border-radius: 8px;"
+            />
+            <div style=" margin-bottom: 50px;">EpicGames.com</div>
+          </td>
+          <td>
+            
+            <img
+              src="../assets/i.png"
+              alt=""
+              style="width: 150px; border-radius: 8px"
+            />
+            <div style="margin-bottom: 150px;">Pinterest.com</div>
+          </td>
+        </tr>
+       
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      x: "Wer ist die beste Freundin der Welt ",
+    };
+  },
+};
+</script>
+
+<style>
+</style>  
+    
